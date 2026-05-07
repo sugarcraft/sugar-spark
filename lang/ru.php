39715525f54a9bf20ac51c3ae45fa4c10a441477
Joe Huss <detain@interserver.net>
2026-05-07 05:52:41 -0400

    Add Russian locale for all libs (#255)

---
+++ lang/ru.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Russian translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "Использование: sugarspark [файл]\n  или:  cmd | sugarspark",
+];
