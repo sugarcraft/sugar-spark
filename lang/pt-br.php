06e1a30287527e4e8799a8a18367061db5489819
Joe Huss <detain@interserver.net>
2026-05-07 05:47:08 -0400

    Add Brazilian Portuguese locale for all libs (#252)

---
+++ lang/pt-br.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Brazilian Portuguese translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "uso: sugarspark [arquivo]\n  ou:  cmd | sugarspark",
+];
