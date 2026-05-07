418325d650792a8eecf6e4679422dba9c6a49e5b
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Korean translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "사용법: sugarspark [파일]\n  또는:  cmd | sugarspark",
+];
