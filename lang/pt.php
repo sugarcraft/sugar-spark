30c61f5272a09f5400aa90d34f0dcbc3c552a5d6
Joe Huss <detain@interserver.net>
2026-05-07 05:45:19 -0400

    Add remaining Portuguese locale files for all libs (#251)

---
+++ lang/pt.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Portuguese translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "uso: sugarspark [ficheiro]\n  ou:  cmd | sugarspark",
+];
