1ca4b318473cc63d613c2cd3fa600a1b1dca1fd6
Joe Huss <detain@interserver.net>
2026-05-07 05:35:41 -0400

    Add French locale for all remaining libs (#247)

---
+++ lang/fr.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * French translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "usage: sugarspark [fichier]\n  ou :  cmd | sugarspark",
+];
