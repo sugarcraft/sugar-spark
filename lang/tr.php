1c7611a284d30182546a546ae6098f091a82a94d
Joe Huss <detain@interserver.net>
2026-05-07 06:02:11 -0400

    Add Turkish locale for all libs (#260)

---
+++ lang/tr.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Turkish translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "Kullanım: sugarspark [dosya]\n  veya:  cmd | sugarspark",
+];
