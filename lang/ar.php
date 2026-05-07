f928b8906fef748a61043538ebca68c817019318
Joe Huss <detain@interserver.net>
2026-05-07 06:07:28 -0400

    Add Arabic locale for all libs (#262)

---
+++ lang/ar.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Arabic translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "الاستخدام: sugarspark [ملف]\n  أو:  cmd | sugarspark",
+];
