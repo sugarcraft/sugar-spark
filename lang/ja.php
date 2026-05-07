0737b73436eac8fa3398bb21ac06b52e972dbf6f
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Japanese translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "用法：sugarspark [ファイル]\n  または：  cmd | sugarspark",
+];
