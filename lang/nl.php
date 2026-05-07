7d1af33fd7c680114f160936cf63c50b5897eb05
Joe Huss <detain@interserver.net>
2026-05-07 06:00:16 -0400

    Add Dutch locale for all libs (#259)

---
+++ lang/nl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Dutch translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "Gebruik: sugarspark [bestand]\n  of:  cmd | sugarspark",
+];
