41b306fdf0bddcb5d11a52f025e92de07b0616a0
Joe Huss <detain@interserver.net>
2026-05-07 05:58:30 -0400

    Add Polish locale for all libs (#258)

---
+++ lang/pl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Polish translations for sugar-spark.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.usage' => "Użycie: sugarspark [plik]\n  lub:  cmd | sugarspark",
+];
